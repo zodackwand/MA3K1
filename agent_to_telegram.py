3db9b5117bb2122e2387e2a1867e2aacaf6a7f1d
Alex <94749662+T0mOrJerry@users.noreply.github.com>
2024-10-27 08:23:04 +0000

    Update agent_to_telegram.py

--- agent_to_telegram.py
+++ agent_to_telegram.py
@@ -3,16 +3,22 @@ import telegram, asyncio
 from uagents.setup import fund_agent_if_low
 from testing_voice_rec import live_chat
 from playsound import playsound
+import contextlib
+from computer_vision import c_vision, choose_media
+
 
 class Message(Model):
     message: str
+
 
 class ContextPrompt(Model):
     context: str
     text: str
 
+
 class Response(Model):
     text: str
+
 
 AGENT_MAILBOX_KEY = "c47b2ef5-5750-439c-9eb9-efe08a5af8bf"
 SEED_PHRASE = "bike test"
@@ -28,9 +34,14 @@ print(agent.address)
 AI_AGENT_ADDRESS = "agent1q0h70caed8ax769shpemapzkyk65uscw4xwk6dc4t3emvp5jdcvqs9xs32y"
 
 boolie = Agent(name="boolie", seed="boolie recovery phrase")
-bob = Agent(name="bob", seed="bob recovery phrase") #he is deciding based on video if need to check
+bob = Agent(name="bob", seed="bob recovery phrase")  # he is deciding based on video if need to check
 
 FILE_NAME = "agent_message.txt"
+MEDIA_FILE_NAME = choose_media()
+c_vision(MEDIA_FILE_NAME)
+with open(FILE_NAME, "w") as f:
+    with contextlib.redirect_stdout(f):
+        c_vision()
 with open(FILE_NAME, "r") as file:
     code = file.read()
 
@@ -44,15 +55,15 @@ prompt = ContextPrompt(
 async def send_message(ctx: Context):
     await ctx.send(AI_AGENT_ADDRESS, prompt)
 
+
 @agent.on_message(Response)
 async def handle_response(ctx: Context, sender: str, msg: Response):
     ctx.logger.info(f"Received response from {sender}: {msg.text}")
     await ctx.send(boolie.address, Message(message=msg.text))
 
 
-
 bot_token = "8182349154:AAG2LT6gGfUHVs2fRXdbtmIaGWqqVPUjI18"
-chat_id=978673064
+chat_id = 978673064
 
 
 async def send_tg(msg, chat_id, token=bot_token):
@@ -60,10 +71,10 @@ async def send_tg(msg, chat_id, token=bot_token):
     await bot.sendMessage(chat_id=chat_id, text=msg)
     print('Message Sent!')
 
- 
-@boolie.on_message(model=Message) #do we need to ask or not
+
+@boolie.on_message(model=Message)  # do we need to ask or not
 async def boolie_message_handler(ctx: Context, sender: str, mssg: Message):
-    playsound("query.mp3")
+    playaudio("query.mp3")
     print("We can start speaking")
     message_text = live_chat()
     if message_text == "True":
@@ -71,25 +82,24 @@ async def boolie_message_handler(ctx: Context, sender: str, mssg: Message):
     else:
         print("all is okay")
 
+
 @bob.on_message(model=Message)
 async def bob_message_handler(ctx: Context, sender: str, mssg: Message):
     if mssg.message == "True":
-        message_text="Help Requested!!"
+        message_text = "Help Requested!!"
         await send_tg(msg=message_text, chat_id=chat_id, token=bot_token)
     else:
         print("Fuck tg bots")
-
 
 
 # Message to send
 
 bureau = Bureau()
 bureau.add(boolie)
-#bureau.add(bob)
+# bureau.add(bob)
 bureau.add(agent)
 bureau.add(bob)
- 
- 
+
 # This constructor simply ensure that only this script is running
 if __name__ == "__main__":
     bureau.run()
